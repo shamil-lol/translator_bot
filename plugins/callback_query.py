d99c810da1382903e4aaf470f1d5b4e6debfb359
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-10 06:00:39 +0530

    Create callback_query.py

---
+++ plugins/callback_query.py
@@ -0,0 +1,19 @@
+from pyrogram import Client as bot
+from pyrogram import filters
+from googletrans import Translator
+
+def translate (text, lang):
+  return Translator().translate(dest = lang, text = text).text
+
+available_langs = ["en", "am", "ar", "es", "ml",  "fr", "tr"]
+@bot.on_callback_query()
+async def callback_query (client, m):
+  try:
+    if m.message.text == translate(m.message.text, m.data):
+      await m.answer(f"{m.message.text} is {m.message.text}")
+    elif m.data in available_langs:
+      await m.answer(f"{m.message.text}   →   {translate(m.message.text, m.data)}", show_alert = True)
+    else:
+      await m.answer("not availabe")
+  except Exception as e:
+    await m.answer(f"{e}")
