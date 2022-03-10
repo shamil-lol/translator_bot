72f5cb1730d2cbb63c018677539b3abcbc74737e
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-10 06:01:58 +0530

    Create inline_query.py

---
+++ plugins/inline_query.py
@@ -0,0 +1,40 @@
+from pyrogram import Client as bot
+from pyrogram import filters
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+from googletrans import Translator
+
+@bot.on_inline_query()
+async def inline_query (client, m):
+    text = m.query
+    try:
+      en = Translator().translate(dest = "en", text = text).text
+      am = Translator().translate(dest = "am", text = text).text
+      ar = Translator().translate(dest = "ar", text = text).text
+      es = Translator().translate(dest = "es", text = text).text                                           fr = Translator().translate(dest = "fr", text = text).text
+    except Exception:
+      pass
+    try:
+      await m.answer(results = [
+      InlineQueryResultArticle(
+      "ENGLISH",
+      InputTextMessageContent(en)
+      ),
+      InlineQueryResultArticle(
+      "AMHARIC",
+      InputTextMessageContent(am)
+      ),
+      InlineQueryResultArticle(
+      "ARABIC",
+      InputTextMessageContent(ar)
+      ),
+      InlineQueryResultArticle(
+      "SPANISH",
+      InputTextMessageContent(es)
+      ),
+      InlineQueryResultArticle(
+      "FRENCH",
+      InputTextMessageContent(fr)
+      )
+      ])
+    except Exception as e:
+      pass
