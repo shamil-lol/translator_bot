cc764a38ca48f93b8ffb5fec36172b3f3f73d002
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-11 19:21:38 +0530

    Update inline_query.py

--- plugins/inline_query.py
+++ plugins/inline_query.py
@@ -6,41 +6,44 @@ from googletrans import Translator
 @bot.on_inline_query()
 async def inline_query (client, m):
     text = m.query
-    try:
-      en = Translator().translate(dest = "en", text = text).text
-      am = Translator().translate(dest = "am", text = text).text
-      ar = Translator().translate(dest = "ar", text = text).text
-      es = Translator().translate(dest = "es", text = text).text                                           
-      fr = Translator().translate(dest = "fr", text = text).text
-      tr = Translator().translate(dest = "tr", text = text).text
-    except Exception:
-      pass
-    try:
-      await m.answer(results = [
-      InlineQueryResultArticle(
-      "ENGLISH",
-      InputTextMessageContent(en)
-      ),
-      InlineQueryResultArticle(
-      "AMHARIC",
-      InputTextMessageContent(am)
-      ),
-      InlineQueryResultArticle(
-      "ARABIC",
-      InputTextMessageContent(ar)
-      ),
-      InlineQueryResultArticle(
-      "SPANISH",
-      InputTextMessageContent(es)
-      ),
-      InlineQueryResultArticle(
-      "FRENCH",
-      InputTextMessageContent(fr)
-      ),
-      InlineQueryResultArticle(
-      "TURKISH",
-      InputTextMessageContent(tr)
-      )
-      ])
-    except Exception as e:
-      pass
+    if m.chat_type == "sender":
+      m.answer(switch_pm_text = "You Cant Use Me In My Chat", switch_pm_parameter = "enc-my_chat", results = [])
+    else:
+      try:
+        en = Translator().translate(dest = "en", text = text).text
+        am = Translator().translate(dest = "am", text = text).text
+        ar = Translator().translate(dest = "ar", text = text).text
+        es = Translator().translate(dest = "es", text = text).text
+        fr = Translator().translate(dest = "fr", text = text).text
+        tr = Translator().translate(dest = "tr", text = text).text
+      except Exception:
+        pass
+      try:
+        await m.answer(results = [
+        InlineQueryResultArticle(
+        "ENGLISH",
+        InputTextMessageContent(en)
+        ),
+        InlineQueryResultArticle(
+        "AMHARIC",
+        InputTextMessageContent(am)
+        ),
+        InlineQueryResultArticle(
+        "ARABIC",
+        InputTextMessageContent(ar)
+        ),
+        InlineQueryResultArticle(
+        "SPANISH",
+        InputTextMessageContent(es)
+        ),
+        InlineQueryResultArticle(
+        "FRENCH",
+        InputTextMessageContent(fr)
+        ),
+        InlineQueryResultArticle(
+        "TURKISH",
+        InputTextMessageContent(tr)
+        )
+        ])
+      except Exception as e:
+        pass
