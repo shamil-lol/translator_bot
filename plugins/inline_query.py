bf39224c06683f5eb941e16ce29599e18a9c0295
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-12 07:10:37 +0530

    Update inline_query.py

--- plugins/inline_query.py
+++ plugins/inline_query.py
@@ -7,7 +7,7 @@ from googletrans import Translator
 async def inline_query (client, m):
     text = m.query
     if m.chat_type == "sender":
-      m.answer(switch_pm_text = "You Cant Use Me In My Chat", switch_pm_parameter = "enc-my_chat", results = [])
+      m.answer(results = [], switch_pm_text = "You Can't Use Me In My Chat, Please Switch To Another Chat", switch_pm_parameter = "nop")
     else:
       try:
         en = Translator().translate(dest = "en", text = text).text
