beb2dea3090671cae1979b47f6983efcff3f4ede
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-12 15:42:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,7 +1,7 @@
 from pyrogram import Client as bot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from utils.deeplink import deep_links
 buttons = InlineKeyboardMarkup([
 [InlineKeyboardButton(text = "USE INLINE MODE", switch_inline_query_current_chat = "")],
 [InlineKeyboardButton(text = "JOIN OUR GROUP", url = "https://t.me/Ethiopians_project"),
@@ -9,4 +9,11 @@ InlineKeyboardButton(text = "CONTACT OWNER", url = "https://t.me/shamil01_bot")]
 ])
 @bot.on_message(filters.private & filters.command('start'))
 async def start (client, m):
-  await m.reply_text(f"hello {m.chat.first_name},\nWelcome to translator bot", reply_markup = buttons)
+  start_message = f"hello {m.chat.first_name},\nWelcome to translator bot"
+  dl = m.text.replace("/start ", "")
+  if dl == m.text:
+    await m.reply_text(start_message, reply_markup = buttons)
+  elif dl in deep_links:
+    await m.reply_text(deep_links[dl])
+  else:
+    await m.reply_text(start_message, reply_markup = buttons)
