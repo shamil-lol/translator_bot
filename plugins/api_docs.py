ca506ffb2f55b22366c9705fb2de2569e57c295a
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-19 17:39:00 +0530

    Update api_docs.py

--- plugins/api_docs.py
+++ plugins/api_docs.py
@@ -3,5 +3,5 @@ from pyrogram import filters
 from utils.deeplink import docs
 
 @bot.on_message(filters.private & filters.command('api'))
-async api_(client, m):
+async def api_(client, m):
   await m.reply_text(docs)
