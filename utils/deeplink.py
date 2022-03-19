a415152e85dfe9f4de176b23130549921bdeac2c
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-19 17:30:23 +0530

    Update deeplink.py

--- utils/deeplink.py
+++ utils/deeplink.py
@@ -1,3 +1,36 @@
+docs = """
+🙂 thank you
+**nodejs**
+```
+// npm i axios
+const axios = require("axios")
+const url = "https://translatorapi-production.up.railway.app/"
+// data must looks like
+const data = {
+  text: "thank you",
+  lang: "es"
+}
+axios.post(url, data).then((data) => {
+  console.log(data) 
+  // or
+  console.log(data.data)
+})
+```
+**python**
+```
+# pip install requests
+import requests
+url = "https://translatorapi-production.up.railway.app/"
+data = {
+  "text": "thank you",
+  "lang": "es"
+}
+response = requests.post(url, data).text
+print (response)
+```
+⚠️ note - we are working on this api for supporting other languages
+"""
 deep_links = {
-  "enc-own_chat": "Please Move To Another Chat"
+  "enc-own_chat": "Please Move To Another Chat",
+  "enc-off_api": docs,
 }
