3ba1238dfa15fd6ef071201611dfa7e0bf1ba496
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-04-19 09:49:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from pyrogram import Client, filters
 import logging
+from os import getenv as env
 logging.basicConfig(level=logging.INFO)
 bot = Client(
-  "trt",
-  config_file = "config.ini"
+  api_id = env("API_ID"),
+  api_hash = env("API_HASH"),
+  bot_token = env("BOT_TOKEN"),
+  session_name = "trt",
+  plugins = { "root": "plugins" }
   )
 bot.run()
